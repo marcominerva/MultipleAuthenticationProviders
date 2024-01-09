7243f1c5222f7e1ef119ab66270185085fdbfc04
Marco Minerva <marco.minerva@gmail.com>
2024-01-09 10:07:13 +0100

    Libraries update

--- MultipleAuthenticationProviders/Authentication/ClaimsTransformer.cs
+++ MultipleAuthenticationProviders/Authentication/ClaimsTransformer.cs
@@ -4,15 +4,8 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace MultipleAuthenticationProviders.Authentication;
 
-public class ClaimsTransformer : IClaimsTransformation
+public class ClaimsTransformer(IMemoryCache memoryCache) : IClaimsTransformation
 {
-    private readonly IMemoryCache memoryCache;
-
-    public ClaimsTransformer(IMemoryCache memoryCache)
-    {
-        this.memoryCache = memoryCache;
-    }
-
     public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
         var identity = (ClaimsIdentity)principal.Identity;
@@ -22,7 +15,7 @@ public class ClaimsTransformer : IClaimsTransformation
             var claims = await memoryCache.GetOrCreateAsync(identity.Name, (entry) =>
             {
                 // Retrieve claims from database...
-                var claimsFromDb = new List<Claim>() { new Claim(ClaimTypes.Role, "Administrator") };
+                var claimsFromDb = new List<Claim>() { new(ClaimTypes.Role, "Administrator") };
 
                 var unixTime = Convert.ToInt64(identity.FindFirst("exp")?.Value);
                 var expirationDateTime = DateTimeOffset.FromUnixTimeSeconds(unixTime);
